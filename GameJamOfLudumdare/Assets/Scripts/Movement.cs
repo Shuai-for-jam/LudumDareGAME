d2d647d2320432b6a4c68944234fefed125b102a
金楷清 <jkq1115282337@gmail.com>
2023-05-01 03:40:24 +0800

    Bottle Rotate & Spline Optimize

--- GameJamOfLudumdare/Assets/Scripts/Movement.cs
+++ GameJamOfLudumdare/Assets/Scripts/Movement.cs
@@ -7,15 +7,24 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public GameObject bottle;
-    public CurvySpline spline1;
+    public CurvySpline Spline1;
+    public CurvySpline Spline2;
+    public CurvySpline Spline3;
+
+    private CurvySpline SplineNow;
 
     void Update()
     {
+        SplineNow = bottle.GetComponent<SplineController>().Spline;
+        if (SplineNow == Spline1)
+            bottle.GetComponent<SplineController>().Clamping = CurvyClamping.Clamp;
+        else
+            bottle.GetComponent<SplineController>().Clamping = CurvyClamping.Loop;
         if (Input.GetKeyDown(KeyCode.S))
         {
-            bottle.GetComponent<SplineController>().Spline = spline1;
+            
         }
-            if (Input.GetKeyDown(KeyCode.R))    //��ͷ
+            if (Input.GetKeyDown(KeyCode.R))    
         {
             if (bottle.GetComponent<SplineController>().MovementDirection == MovementDirection.Forward)
                 bottle.GetComponent<SplineController>().MovementDirection = MovementDirection.Backward;
