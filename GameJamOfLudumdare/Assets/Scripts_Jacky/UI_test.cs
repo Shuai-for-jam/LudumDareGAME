dde25486cb09784d2934e9c1957150ca33bc1c84
Abyssjac <zuzhou.abyss@gmail.com>
2023-04-30 23:25:28 +0800

    UI_test
    试试先

--- GameJamOfLudumdare/Assets/Scripts_Jacky/UI_test.cs
+++ GameJamOfLudumdare/Assets/Scripts_Jacky/UI_test.cs
@@ -7,15 +7,26 @@ public class UI_test : MonoBehaviour
 {
     public Image skillA, skillB;
     public float mana;
+    float speed_mana=10f;
+    bool canSkillA,canSkillB;
     void Start()
     {
         mana = 0;
     }
     void Update()
     {
-        mana+=Time.deltaTime*5;
+        mana+=Time.deltaTime*speed_mana;
         skillB.color = new Color(1, 1, 1, mana/100);
         //Debug.Log(skillB.color);
         //skillB.fillAmount = mana / 100f;
+        if (canSkillA == false)
+        {
+            mana+= Time.deltaTime*speed_mana;
+        }
+        if (mana >= 100)
+        {
+            canSkillA = true;
+        }
+
     }
 }
--- GameJamOfLudumdare/Assets/Scripts_Jacky/collision_bottle.cs
+++ GameJamOfLudumdare/Assets/Scripts_Jacky/collision_bottle.cs
@@ -1,26 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class collision_bottle : MonoBehaviour
 {
-    public float mana;
+
+    bool canInteract;
+    public GameObject characA, characB, characC, characD, characE;
+    public float distance;
+    public Image characAi;
     void Start()
     {
-        mana =0;
+        characA = GameObject.FindGameObjectWithTag("characA");
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(transform.position, characA.transform.position) <= distance&&canInteract==false)
+        {
+            characA.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                canInteract = true;
+            }
+        }
+        //瓶子和角色距离小于定值的时候，使角色头上的感叹号（characA）出现，此时若按下Q，可对UI进行操控
+        if (Vector3.Distance(transform.position, characA.transform.position) > distance && canInteract == false)
+        {
+            characA.SetActive(false);
+        }
+        //距离大于定值，感叹号（characA）不出现
+
+        if (canInteract == true)
+        {
+            Time.timeScale = 0;
+            //先使时间暂停
+            characAi.enabled=true;
+            //UI图片出现
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                characAi.enabled = false;
+                Time.timeScale = 1;
+            }
+        }   
+
+
         
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("character"))
-        {
-            Debug.Log("1");
-            mana += 20;
-        }
-    }
+
 }
