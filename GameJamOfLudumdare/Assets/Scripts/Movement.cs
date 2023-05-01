b51a06d308594176ba519d080bb42b55b0177047
金楷清 <jkq1115282337@gmail.com>
2023-05-01 17:11:21 +0800

    Smooth Transitions!

--- GameJamOfLudumdare/Assets/Scripts/Movement.cs
+++ GameJamOfLudumdare/Assets/Scripts/Movement.cs
@@ -16,6 +16,8 @@ public class Movement : MonoBehaviour
 
     private CurvySpline SplineNow;
 
+    Vector3 nearestPos;
+    float nearestTF;
 
     void Update()
     {
@@ -31,20 +33,18 @@ public class Movement : MonoBehaviour
             // 将target的坐标转换到spline所在的本地坐标系  
             var targetPos = Spline3.transform.InverseTransformPoint(target.position);
             // 获得target在spline上的最近点的TF值  
-            float nearestTF = Spline3.GetNearestPointTF(targetPos);
+            nearestTF = Spline3.GetNearestPointTF(targetPos);
             // 转换到世界坐标系的spline上最近点的坐标值  
-            var nearestPos = Spline3.transform.TransformPoint(Spline3.Interpolate(nearestTF));
+            nearestPos = Spline3.transform.TransformPoint(Spline3.Interpolate(nearestTF));
 
 
             float distance = Vector3.Distance(target.position, nearestPos);
             Debug.Log(distance);
             if (distance <= MaxDistance)
             {
-                //bottle.GetComponent<SplineController>().Spline = null;
-                //bottle.transform.position = Vector3.MoveTowards(transform.position, nearestPos, Time.deltaTime * TransSpeed);
+                bottle.GetComponent<SplineController>().Spline = null;
+                StartCoroutine(MoveToNearestPos());
 
-                bottle.GetComponent<SplineController>().Spline = Spline3;
-                bottle.GetComponent<SplineController>().Position = nearestTF;
 
 
             }
@@ -61,5 +61,24 @@ public class Movement : MonoBehaviour
                 bottle.GetComponent<SplineController>().MovementDirection = MovementDirection.Backward;
             else bottle.GetComponent<SplineController>().MovementDirection = MovementDirection.Forward;
         }
+    }
+
+    IEnumerator MoveToNearestPos()
+    {
+        // 计算物体到目标的距离
+        float distanceToTarget = Vector3.Distance(transform.position, nearestPos);
+
+        // 当距离大于一个很小的值（如0.001）时，继续移动物体
+        while (distanceToTarget > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nearestPos, TransSpeed * Time.deltaTime);
+            distanceToTarget = Vector3.Distance(transform.position, nearestPos);
+            yield return null; // 等待下一帧再继续执行
+        }
+
+        // 物体到达目标后，执行后续函数
+
+        bottle.GetComponent<SplineController>().Spline = Spline3;
+        bottle.GetComponent<SplineController>().Position = nearestTF;
     }
 }
